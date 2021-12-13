61e1cd1cba1897b63f5a70fe4d345ed0676cd439
Moritz Reiter <reiter@b1-systems.de>
2021-12-13 15:10:15 +0000

    SimpleFormatterTest

--- test/Formatter/SimpleFormatterTest.php
+++ test/Formatter/SimpleFormatterTest.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * Horde Log package
- *
- * This package is based on Zend_Log from the Zend Framework
- * (http://framework.zend.com).  Both that package and this
- * one were written by Mike Naberezny and Chuck Hagenbuch.
- *
- * @author     Mike Naberezny <mike@maintainable.com>
- * @author     Chuck Hagenbuch <chuck@horde.org>
- * @category   Horde
- * @license    http://www.horde.org/licenses/bsd BSD
- * @package    Log
- * @subpackage UnitTests
- */
+
 namespace Horde\Log\Test\Formatter;
 use PHPUnit\Framework\Testcase;
 use Horde\Log\Formatter\SimpleFormatter;
@@ -21,32 +8,69 @@ use horde\Log\LogFormatter;
 use Horde\Log;
 
 
-/**
- * @author     Mike Naberezny <mike@maintainable.com>
- * @author     Chuck Hagenbuch <chuck@horde.org>
- * @category   Horde
- * @license    http://www.horde.org/licenses/bsd BSD
- * @package    Log
- * @subpackage UnitTests
- */
-class SimpleFormatterTest extends Testcase
+
+class SimpleFormatterTest extends TestCase
 {
-    public function testConstructorThrowsOnBadFormatString()
+    public  function testConstructorThrowsOnBadFormatString()
     {
-        $this->expectException('InvalidArgumentException');    
+        $this->expectException('InvalidArgumentException');
         new SimpleFormatter(1);
-    }
-
-    public function testDefaultFormat()
-    {
-        $f = new SimpleFormatter(2);
-        $line = $f->format(array('message' => $message = 'message',
-                                 'level' => $level = Horde\Log::ALERT,
-                                 'levelName' => $levelName = 'ALERT'));
-
-        $this->assertStringContainsString($message, $line);
-        $this->assertStringContainsString($levelName, $line);
     }
 }
 
-?>
+class ShouldNeverBeRunEither extends SimpleFormatterTest
+{
+}
+
+class TestOfStackTrace extends TestCase
+{
+    public function testCanFindAssertInTrace()
+    {
+        $trace = new SimpleStackTrace(['assert']);
+        $this->assertEqual(
+            $trace->traceMethod([[
+                'file' => 'TestCase',
+                'line' => 24,
+                'function' => 'assertSomething', ]]),
+                ' at [TestCase line 24]'
+            );
+    }
+}
+
+class DummyResource
+{
+}
+
+class TestOfContext extends TestCase
+{
+    public function testCurrentContextIsUnique()
+    {
+        $this->assertSame(
+            SimpleFormatterTest::getContext(),
+            SimpleFormatterTest::getContext(1)
+        );
+    }
+
+    public function testContextHoldsCurrentTestCase()
+    {
+        $context = SimpleFormatterTest::getContext();
+        $this->assertSame($this, $context->getTest());
+    }
+
+    public function testResourceIsSingleInstanceWithContext()
+    {
+        $context = new SimpleTestContext();
+        $this->assertSame(
+            $context->get('DummyResource'),
+            $context->get('DummyResource')
+        );
+    }
+
+    public function testClearingContextResetsResources()
+    {
+        $context = new SimpleTestContext(1);
+        $resource = $context->get('DummyResource');
+        $context->clear();
+        $this->assertClone($resource, $context->get('DummyResource'));
+    }
+}
