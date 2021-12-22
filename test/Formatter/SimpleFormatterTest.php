f9109fb44a630801be961298e77ed84c52f7208f
Moritz Reiter <reiter@b1-systems.de>
2021-12-22 09:12:57 +0000

    wip SimpleFormatterTest

--- test/Formatter/SimpleFormatterTest.php
+++ test/Formatter/SimpleFormatterTest.php
@@ -12,24 +12,51 @@ use Horde\Log\Formatter\SimpleFormatter;
 use Horde\Log\LogMessage;
 use Horde\Log\LogFormatter;
 
+/*class SimpleFormatterTest extends TestCase
 
-class SimpleFormatterTest extends TestCase
-{
+
+    
         public function setup(): void
         {
             $this->filter = new SimpleFormatter();
         }
 
-        public function Formatisnull()
+        public function testFormatisnull()
         {
             $this->assertTrue($this->filter->accept(array('message' => '', 'level' === 0)));
+            ->assertNull
         }
 
-        public function Formatisnotnull()
+        public function testFormatisnotnull()
         {
-            $this->assertFalse($this->filter->accept(array('message' => '', 'level' != 1)));
+            /*$this->assertFalse($this->filter->accept(array('message' => '', 'level' != 1)));
+            ->assertNotNull
         }
+    
+    } */
+    
+    class SimpleFormatterTest1 extends LogMessage
+    {
+
         
+         
+        public function testDefaultFormat()
+    {
+        $f = new SimpleFormatter();
+        $line = $f->format(array('context' => $context = 'context', 'name' => $name = 1));
+        
+        $this->assertStringContainsString($context, $line);
+        $this->assertStringContainsString((string)$name, $line);
+    } 
+
+    public function testDeclarationIsStripped()
+    {
+        $f = new SimpleFormatter();
+        $line = $f->format(array('context' => $context = 'context', 'name' => $name = 0));
+
+        $this->assertStringNotContainsString ('Hallo', $line);
+    }
+
         public function testConstructorThrowsOnBadFormatString()
     {
         $this->expectException('InvalidArgumentException');
@@ -44,5 +71,3 @@ class SimpleFormatterTest extends TestCase
         new SimpleFormatter(5);
     }
 }
-
-?>
