30c610714b0244545d7e2d32b35b6786248a9d1e
Moritz Reiter <reiter@b1-systems.de>
2021-12-23 15:35:37 +0000

    SimpleFormatterTest finished

--- test/Formatter/SimpleFormatterTest.php
+++ test/Formatter/SimpleFormatterTest.php
@@ -1,63 +1,40 @@
 <?php
-/**
- * @author     Moritz Reiter
+
+ /* @author     Moritz Reiter
  * @category   Horde
  * @package    Log
- * @subpackage UnitTests
- */
+ * @subpackage UnitTests*/
 
-namespace Horde\Log\Test\Formatter;
-use PHPUnit\Framework\Testcase;
-use Horde\Log\Formatter\SimpleFormatter;
-use Horde\Log\LogMessage;
-use Horde\Log\LogFormatter;
+    namespace Horde\Log\Test\Formatter;
+    use PHPUnit\Framework\Testcase;
+    use Horde\Log\Formatter\SimpleFormatter;
+    use Horde\Log\LogMessage;
+    use Horde\Log\LogLevel;
 
-/*class SimpleFormatterTest extends TestCase
+    class SimpleFormatterTest extends TestCase
 {
-
     
-        public function setup(): void
-        {
-            $this->filter = new SimpleFormatter();
-        }
+    public function testDefaultFormat()
 
-        public function testFormatisnull()
-        {
-            $this->assertTrue($this->filter->accept(array('message' => '', 'level' === 0)));
-            ->assertNull
-        }
-
-        public function testFormatisnotnull()
-        {
-            /*$this->assertFalse($this->filter->accept(array('message' => '', 'level' != 1)));
-            ->assertNotNull
-        }
-    
-    } */
-    
-    class SimpleFormatterTest1 extends LogMessage
     {
-    
-        public function testDefaultFormat()
-    {
-        $f = new SimpleFormatter();
-        $line = $f->format(array('context' => $context = 'context', 'name' => $name = 1));
-        
-        $this->assertStringContainsString($context, $line);
-        $this->assertStringContainsString((string)$name, $line);
-    } 
 
-    public function testDeclarationIsStripped()
-    {
-        $f = new SimpleFormatter();
-        $line = $f->format(array('context' => $context = 'context', 'name' => $name = 0));
+    $f = new SimpleFormatter('%message% %context%');
 
-        $this->assertStringNotContainsString ('Hallo', $line);
+    $context = 'testValue';
+
+    $logLevel = new LogLevel(1, 'testLevel');
+
+    $message = 'test message';
+
+    $logMessage = new LogMessage($logLevel, $message, ['context' => $context ]);
+
+    $logMessage->formatMessage([]);
+
+    $line = $f->format($logMessage);
+
+    /*var_dump ($line);*/
+
+    $this->assertEquals('test message testValue', $line);
+
     }
-
-        public function testConstructorThrowsOnBadFormatString()
-    {
-       $this->expectException(TypeError::class);
-       new SimpleFormatter();
-    } 
 }
